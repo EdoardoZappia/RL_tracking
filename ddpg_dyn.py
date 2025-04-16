a8563ee7d732638d35e4fb2b05a33688fad18908
EdoardoZappia <edoardozappia5@gmail.com>
2025-04-16 17:14:33 +0200

    m

--- ddpg_dyn.py
+++ ddpg_dyn.py
@@ -44,11 +44,11 @@ class PolicyNet(nn.Module):
         # Per gestire batch e singoli stati
         if training:
             if state.dim() == 1:
-                noise = torch.normal(mean=0.0, std=0.005, size=(2,), device=state.device)
+                noise = torch.normal(mean=0.0, std=0.001, size=(2,), device=state.device)
                 state = state.clone()
                 state[2:4] = state[2:4] + noise
             else:
-                noise = torch.normal(mean=0.0, std=0.005, size=state[:, 2:4].shape, device=state.device)
+                noise = torch.normal(mean=0.0, std=0.001, size=state[:, 2:4].shape, device=state.device)
                 state = state.clone()
                 state[:, 2:4] = state[:, 2:4] + noise
 
@@ -72,11 +72,11 @@ class QNet(nn.Module):
         # Per gestire batch e singoli stati
         if training:
             if state.dim() == 1:
-                noise = torch.normal(mean=0.0, std=0.005, size=(2,), device=state.device)
+                noise = torch.normal(mean=0.0, std=0.001, size=(2,), device=state.device)
                 state = state.clone()
                 state[2:4] = state[2:4] + noise
             else:
-                noise = torch.normal(mean=0.0, std=0.005, size=state[:, 2:4].shape, device=state.device)
+                noise = torch.normal(mean=0.0, std=0.001, size=state[:, 2:4].shape, device=state.device)
                 state = state.clone()
                 state[:, 2:4] = state[:, 2:4] + noise
 
