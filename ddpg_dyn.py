ad6249c5545548b8f271e6cc210c4be84ab2519d
EdoardoZappia <edoardozappia5@gmail.com>
2025-04-16 18:27:01 +0200

    m

--- ddpg_dyn.py
+++ ddpg_dyn.py
@@ -16,7 +16,7 @@ torch.manual_seed(SEED)
 np.random.seed(SEED)
 
 NUM_NEURONS = 256
-LR_ACTOR = 0.0005   #0.001
+LR_ACTOR = 0.0001   #0.001
 LR_CRITIC = 0.001    #0.0008  #0.001
 GAMMA = 0.99
 TAU = 0.005
